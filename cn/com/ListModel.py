637b518443f170f711cd4773a68832d2974631e1
luocong2013 <luocong2013@outlook.com>
2018-01-08 22:32:39 +0800

    图形界面编程

--- cn/com/ListModel.py
+++ cn/com/ListModel.py
@@ -7,3 +7,15 @@ print(list[1:3])
 print(list[2:])
 print(tinylist * 2)
 print(list + tinylist)
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+# 打印Apple:
+print(L[0][0])
+# 打印Python:
+print(L[1][1])
+# 打印Lisa:
+print(L[2][2])
