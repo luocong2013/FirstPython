779124bbca38fde544c14c6042a00343ecf98f50
luocong <luocong2013@outlook.com>
2020-02-13 17:46:04 +0800

    半成品

--- cn/crawler/test/test_selenium.py
+++ cn/crawler/test/test_selenium.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+import requests
 """
 driver.page_source （查看网页源码）
 driver.get_cookies() （获取当前浏览器的全部cookies）
@@ -46,7 +46,7 @@ def main():
     options.add_argument('--headless')
     # 可选项：禁用gpu，可以解决一些莫名的问题
     options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(executable_path='D:/mywork/pycharm_workspace/FirstPython/driver/chromedriver.exe', options=options)
+    driver = webdriver.Chrome(executable_path='E:/temp/chromedriver.exe', options=options)
 
     driver.get('https://music.163.com/#/discover/artist')
 
@@ -57,6 +57,18 @@ def main():
     eles = driver.find_elements_by_xpath('//div[@class="g-wrap"]/div[@class="m-sgerlist"]/ul/li/div/a[@class="msk"]')
     for ele in eles:
         href = ele.get_attribute('href')
+        driver.get(href)
+        g_frame = driver.find_element_by_id('g_iframe')
+        driver.switch_to.frame(g_frame)
+        mas = driver.find_elements_by_xpath('//td[2]/div/div/div/span/span')
+        for ma in mas:
+            mid = ma.get_attribute('data-res-id')
+            if mid is not None:
+                singer_url = 'http://music.163.com/song/media/outer/url?id={}.mp3'.format(mid)
+                response = requests.get(singer_url)
+                buff = response.content
+                with open('E:/temp/music/{}.mp3'.format(mid), 'wb') as f:
+                    f.write(buff)
         print(href)
     # 切换到正常页面
     current_windows = driver.window_handles
